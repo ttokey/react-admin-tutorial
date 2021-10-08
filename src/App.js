86271694d0fe984ff6dc4f11b228caf956c827b2
이석희 <ttokey.lee@gmail.com>
2021-10-08 16:00:33 +0900

    이것 저것 해보는 중

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import React from 'react';
 import {Admin, Resource} from 'react-admin';
+import Dashboard from "./Dashboard";
+import authProvider from "./service/authProvider";
 import jsonServerProvider from 'ra-data-json-server';
 import {UserList} from "./component/users";
 import {PostCreate, PostEdit, PostList} from "./component/posts";
-import UserIcon from '@material-ui/icons/Group';
-import PostIcon from '@material-ui/icons/Book';
-import Dashboard from "./Dashboard";
-import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const envInfoProvider = jsonServerProvider('http://localhost:1234');
 
 const App = () => (
     <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
-        <Resource name="users" list={UserList} icon={UserIcon}/>
-        <Resource name="posts" list={PostList} icon={PostIcon} edit={PostEdit} create={PostCreate}/>
-        {/*{<Resource name="envInfo" list={envInfos}/>}*/}
+        <Resource name="users" list={UserList}/>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+        {/*<Resource name="envInfo" list={envInfos}/>*/}
     </Admin>
-
 );
 
 export default App;
--- src/component/posts.js
+++ src/component/posts.js
@@ -9,11 +9,9 @@ import {
     ReferenceInput,
     SelectInput,
     SimpleForm,
-    SimpleList,
     TextField,
     TextInput
 } from 'react-admin';
-import {useMediaQuery} from "@material-ui/core";
 
 const PostTitle = ({record}) => {
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
@@ -28,25 +26,17 @@ const postFilters = [
 
 
 export const PostList = props => {
-    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
-    return (<List filters={postFilters} {...props}>
-            {isSmall ? (
-                <SimpleList
-                    primaryText={record => record.title}
-                    secondaryText={record => `${record.views} views`}
-                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
-                />
-            ) : (
-                <Datagrid>
-                    <TextField source="id"/>
-                    <ReferenceField label="User" source="userId" reference="users">
-                        <TextField source="name"/>
-                    </ReferenceField>
-                    <TextField source="title"/>
-                    <TextField source="body"/>
-                    <EditButton/>
-                </Datagrid>
-            )}
+    return (
+        <List filters={postFilters} {...props}>
+            <Datagrid>
+                <TextField source="id"/>
+                <ReferenceField label="User" source="userId" reference="users">
+                    <TextField source="name"/>
+                </ReferenceField>
+                <TextField source="title"/>
+                <TextField source="body"/>
+                <EditButton/>
+            </Datagrid>
         </List>
     );
 };
