c8095048cf584ae1fbb7afc8cae313bd8d9b1142
이석희 <ttokey.lee@gmail.com>
2021-10-20 16:07:50 +0900

    feat : show diff complete

---
+++ src/component/DiffModal.js
@@ -0,0 +1,97 @@
+import * as React from "react";
+import {
+    CButton,
+    CCol,
+    CForm,
+    CFormLabel,
+    CFormText,
+    CModal,
+    CModalBody,
+    CModalFooter,
+    CModalTitle,
+    CRow
+} from "@coreui/react";
+
+export const DiffModal = (props) => {
+    console.log("props", props);
+
+    return (
+        <CModal size="xl" visible={props.visible} onClose={() => props.setVisible(false)}>
+            <CModalTitle>edit {props.env}</CModalTitle>
+            <CModalBody>
+                <CForm>
+                    <CRow className="mb-3">
+                        <CCol md={2}>
+                            <CFormLabel> id</CFormLabel>
+                        </CCol>
+                        <CCol md={10}>
+                            <CFormText>{props.response.id}</CFormText>
+                        </CCol>
+                    </CRow>
+
+                    <CRow className="mb-3">
+                        <CCol md={2}>
+                            <CFormLabel> fields</CFormLabel>
+                        </CCol>
+                        <CCol md={10}>
+                            {/*<showField/>*/}
+                        </CCol>
+                    </CRow>
+
+                    <CRow className="mb-3">
+                        <CCol md={2}>
+                            <CFormLabel> status</CFormLabel>
+                        </CCol>
+                        <CCol md={10}>
+                            <CFormText>{props.response.status}</CFormText>
+                        </CCol>
+                    </CRow>
+
+                    <CRow className="mb-3">
+                        <CCol md={4}>
+                            <CFormLabel> sourceData</CFormLabel>
+                        </CCol>
+                        <CCol md={4}>
+                            <CFormLabel> targetData</CFormLabel>
+                        </CCol>
+                        <CCol md={4}>
+                            <CFormLabel> diffData</CFormLabel>
+                        </CCol>
+                    </CRow>
+
+                    <CRow className="mb-3">
+                        <CCol md={4}>
+                            <CFormText>
+                                {
+                                    props.response.sourceData.split('\n').map(line => {
+                                        return (<span>{line}<br/></span>)
+                                    })
+                                }
+                            </CFormText>
+                        </CCol>
+                        <CCol md={4}>
+                            <CFormText>
+                                {
+                                    props.response.targetData.split('\n').map(line => {
+                                        return (<span>{line}<br/></span>)
+                                    })
+                                }
+                            </CFormText>
+                        </CCol>
+                        <CCol md={4}>
+                            <CFormText dangerouslySetInnerHTML={{__html: props.response.diffData}}></CFormText>
+                        </CCol>
+                    </CRow>
+                </CForm>
+            </CModalBody>
+            <CModalFooter>
+                <CButton color="secondary" onClick={() => props.setVisible(false)}>
+                    확인
+                </CButton>
+            </CModalFooter>
+        </CModal>
+    )
+
+}
+
+
--- src/component/diffAndTransfer.js
+++ src/component/diffAndTransfer.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import {useEffect, useState} from 'react';
 import {CButton, CCol, CContainer, CRow} from "@coreui/react";
 import Select from "react-select";
-import {getDiffStatusList} from "../service/transferDataProvider";
-import {MDBDataTableV5} from "mdbreact";
-
+import {getDiff, getDiffStatusList} from "../service/transferDataProvider";
+import DataTable from 'react-data-table-component'
+import {DiffModal} from "./DiffModal";
 
 export const DiffAndTransfer = (props) => {
     const [column, setColumn] = useState([]);
@@ -12,8 +12,9 @@ export const DiffAndTransfer = (props) => {
     const [collection, setCollection] = useState('');
     const [sourceEnv, setSourceEnv] = useState('');
     const [targetEnv, setTargetEnv] = useState('');
-    const [checkbox1, setCheckbox1] = useState([]);
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
+    const [visible, setVisible] = useState(false);
+    const [diffData, setDiffData] = useState({});
 
     const handleSelect = (value, setFunction) => {
         setFunction(value.realValue);
@@ -25,61 +26,57 @@ export const DiffAndTransfer = (props) => {
         [collection, sourceEnv, targetEnv]
     )
 
-    useEffect(() => {
 
-            console.log("diffStatus : ", diffStatus);
-            setData({
-                    columns: [
-                        {
-                            label: 'id',
-                            field: 'id',
-                            sort: 'asc',
-                            width: 200,
-                            attributes: {
-                                'aria-controls': 'DataTable',
-                                'aria-label': 'Name',
-                            },
-                        },
-                        {
-                            label: 'nluId',
-                            field: 'fields.nluId',
-                            sort: 'asc',
-                            width: 200
-                        },
-                        {
-                            label: 'confidenceCutScore',
-                            field: 'fields.confidenceCutScore',
-                            sort: 'asc',
-                            width: 200
-                        },
-                        {
-                            label: 'url',
-                            field: 'status',
-                            sort: 'asc',
-                            width: 200
-                        },
-                    ],
-                    rows: diffStatus
-                }
-            );
+    useEffect(() => {
+            setColumn(makeColumns);
+            setData(diffStatus);
+            console.log("data : ", data);
+            console.log("column : ", column);
         }, [diffStatus]
     )
 
-    useEffect(() => {
-            console.log("data : ", data);
+    const makeColumns = [
+        {
+            name: 'id',
+            selector: row => row.id,
+            sortable: true,
+
         },
-        [data]
-    )
+        {
+            name: 'status',
+            selector: row => row.status,
+            sortable: true,
+        },
+        {
+            name: 'nluId',
+            selector: row => row.fields.nluId,
+            sortable: true,
+        },
+        {
+            name: 'confidenceCutScore',
+            selector: row => row.fields.confidenceCutScore,
+            sortable: true,
+        },
+    ];
+
+
+    // data provides access to your row data
+    const ExpandedComponent = ({data}) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+
+    const diffShow = async (row, event) => {
+        const response = await getDiff(collection, row.id, sourceEnv, targetEnv);
+        setVisible(true);
+        // console.log(response);
+        setDiffData(response);
+    };
+
 
     const getDiffStatus = async () => {
         const response = await getDiffStatusList(collection, sourceEnv, targetEnv);
-        console.log(response);
-        setDiffStatus(response);
+        console.log(response.list);
+        setDiffStatus(response.list);
     };
 
-    const showLogs2 = (e) => {
-        setCheckbox1(e);
-    };
 
     return (
         <CContainer>
@@ -111,28 +108,23 @@ export const DiffAndTransfer = (props) => {
                 </CCol>
             </CRow>
 
-            <MDBDataTableV5
-                hover
-                entriesOptions={[5, 20, 25]}
-                entries={5}
-                pagesAmount={4}
+            <DataTable
+                columns={column}
                 data={data}
-                // checkbox
-                //
-                // headCheckboxID='id6'
-                // bodyCheckboxID='checkboxes6'
-                // getValueCheckBox={(e) => {
-                //     showLogs2(e);
-                // }}
-                // getValueAllCheckBoxes={(e) => {
-                //     showLogs2(e);
-                // }}
-                // multipleCheckboxes
+                selectableRows
+                // expandableRows
+                pagination
+                onRowClicked={diffShow}
+                // expandableRowsComponent={ExpandedComponent2}
             />
+
+            <DiffModal
+                visible={visible}
+                setVisible={setVisible}
+                response={diffData}/>
         </CContainer>
     );
 }
-
 
 const collectionOption = [
     {value: 'nlu', label: 'nlu', realValue: 'nlu'},
